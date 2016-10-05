d348e30fa097ec82ac84331771cd098c2033bd5f
Juanito Fatas <katehuang0320@gmail.com>
2016-10-05 20:18:55 +0800

    Make it passed

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,10 @@
 class Bottles
   def verse(number)
-    "99 bottles of beer on the wall, 99 bottles of beer.\n" \
-    "Take one down and pass it around, 98 bottles of beer on the wall.\n"
+    "#{bottles_text(number)} of beer on the wall, #{bottles_text(number)} of beer.\n" \
+    "Take one down and pass it around, #{bottles_text(number - 1)} of beer on the wall.\n"
+  end
+
+  def bottles_text(number)
+    "#{number} bottles"
   end
 end
--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -21,16 +21,5 @@ Take one down and pass it around, 88 bottles of beer on the wall.
         expect(expected).to eq(Bottles.new.verse(89))
       end
     end
-
-    context "2" do
-      it "returns correct verse" do
-        expected = <<-VERSE
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-        VERSE
-
-        expect(expected).to eq(Bottles.new.verse(2))
-      end
-    end
   end
 end

66c616a1a4c741c784912604d713627a0d9156ed
Juanito Fatas <katehuang0320@gmail.com>
2016-10-05 20:08:58 +0800

    Add verse specs

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -1,2 +1,14 @@
 RSpec.describe Bottles do
+  describe "#verse" do
+    context "99" do
+      it "returns correct verse" do
+        expected = <<-VERSE
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+        VERSE
+
+        expect(expected).to eq(Bottles.new.verse(99))
+      end
+    end
+  end
 end

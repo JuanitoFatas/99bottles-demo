ac2bbd58958528b0e7428542b2d3b58082a6597f
Juanito Fatas <katehuang0320@gmail.com>
2016-10-05 20:24:52 +0800

    Add failing spec for couple verses

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -55,4 +55,19 @@ Go to the store and buy some more, 99 bottles of beer on the wall.
       end
     end
   end
+
+  describe "#verses" do
+    context "99 and 98" do
+      it "returns correct verses" do
+        expected = <<-VERSES
+99 bottles of beer on the wall, 99 bottles of beer.
+Take one down and pass it around, 98 bottles of beer on the wall.
+98 bottles of beer on the wall, 98 bottles of beer.
+Take one down and pass it around, 97 bottles of beer on the wall.
+        VERSES
+
+        expect(expected).to eq(Bottles.new.verses(99, 98))
+      end
+    end
+  end
 end

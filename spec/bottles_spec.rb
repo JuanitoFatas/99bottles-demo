6c5e8eedb55b465046f0aa661a3d1fc71d92a537
Juanito Fatas <katehuang0320@gmail.com>
2016-10-05 20:10:21 +0800

    Add specs to make it failed

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -10,5 +10,27 @@ Take one down and pass it around, 98 bottles of beer on the wall.
         expect(expected).to eq(Bottles.new.verse(99))
       end
     end
+
+    context "89" do
+      it "returns correct verse" do
+        expected = <<-VERSE
+89 bottles of beer on the wall, 89 bottles of beer.
+Take one down and pass it around, 88 bottles of beer on the wall.
+        VERSE
+
+        expect(expected).to eq(Bottles.new.verse(89))
+      end
+    end
+
+    context "2" do
+      it "returns correct verse" do
+        expected = <<-VERSE
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+        VERSE
+
+        expect(expected).to eq(Bottles.new.verse(2))
+      end
+    end
   end
 end
